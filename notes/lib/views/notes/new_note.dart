dc11342b0b688572ca68f0a9525eadf66c3cd8c0
Ng'ash_254 <samuelnganga.w@gmail.com>
2024-01-02 19:24:45 +0300

    Add note on new note page

--- notes/lib/views/notes/new_note.dart
+++ notes/lib/views/notes/new_note.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:notes/services/auth/auth_service.dart";
+import "package:notes/services/crud/notes_service.dart";
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({super.key});
@@ -8,6 +10,21 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+  DatabaseNote? _note;
+  late final NoteService _noteService;
+  late final TextEditingController _textController;
+
+  Future<DatabaseNote> createNewNote() async {
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _noteService.getUser(email: email);
+    return await _noteService.createNote(owner: owner);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
