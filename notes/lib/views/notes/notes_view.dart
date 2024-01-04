a917d88d3cd6724e7a9825564bf8f8f255fb2392
Ng'ash_254 <samuelnganga.w@gmail.com>
2024-01-04 20:03:08 +0300

    Navigate to notes view upon tap

--- notes/lib/views/notes/notes_view.dart
+++ notes/lib/views/notes/notes_view.dart
@@ -76,11 +76,16 @@ class _NotesViewState extends State<NotesView> {
                         if (snapshot.hasData) {
                           final allNotes = snapshot.data as List<DatabaseNote>;
                           return NotesListView(
-                            notes: allNotes,
-                            onDeleteNote: (note) async {
-                              await _noteService.deleteNote(id: note.id);
-                            },
-                          );
+                              notes: allNotes,
+                              onDeleteNote: (note) async {
+                                await _noteService.deleteNote(id: note.id);
+                              },
+                              onTap: (note) {
+                                Navigator.of(context).pushNamed(
+                                  createOrUpdateNoteRoute,
+                                  arguments: note,
+                                );
+                              });
                         } else {
                           return const CircularProgressIndicator();
                         }
