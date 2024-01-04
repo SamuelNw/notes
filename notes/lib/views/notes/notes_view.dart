cbfaa06ebfda5fed4631d030e4823fcf14b8e732
Ng'ash_254 <samuelnganga.w@gmail.com>
2024-01-04 14:12:08 +0300

    Use the new listview

--- notes/lib/views/notes/notes_view.dart
+++ notes/lib/views/notes/notes_view.dart
@@ -4,6 +4,7 @@ import "package:notes/enums/menu_action.dart";
 import "package:notes/services/auth/auth_service.dart";
 import "package:notes/services/crud/notes_service.dart";
 import "package:notes/utilities/dialogs/logout_dialog.dart";
+import "package:notes/views/notes/notes_list_view.dart";
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -74,18 +75,10 @@ class _NotesViewState extends State<NotesView> {
                       case ConnectionState.active:
                         if (snapshot.hasData) {
                           final allNotes = snapshot.data as List<DatabaseNote>;
-                          return ListView.builder(
-                            itemCount: allNotes.length,
-                            itemBuilder: (context, index) {
-                              final note = allNotes[index];
-                              return ListTile(
-                                title: Text(
-                                  note.text,
-                                  maxLines: 1,
-                                  softWrap: true,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              );
+                          return NotesListView(
+                            notes: allNotes,
+                            onDeleteNote: (note) async {
+                              await _noteService.deleteNote(id: note.id);
                             },
                           );
                         } else {
