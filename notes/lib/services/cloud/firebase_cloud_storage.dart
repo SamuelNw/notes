3254636062f1cb045188a5747db619b4f77acdd9
Ng'ash_254 <samuelnganga.w@gmail.com>
2024-01-07 13:58:43 +0300

    Function to create new note

--- notes/lib/services/cloud/firebase_cloud_storage.dart
+++ notes/lib/services/cloud/firebase_cloud_storage.dart
@@ -1,7 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:notes/services/cloud/cloud_storage_constants.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection("notes");
+
+  // Create a new note:
+  void createNewNote({required String ownerUserId}) async {
+    notes.add({
+      ownerUserIdFieldName: ownerUserId,
+      textFieldName: "",
+    });
+  }
 
   static final FirebaseCloudStorage _shared = FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance();
