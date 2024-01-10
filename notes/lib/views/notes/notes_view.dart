fcf78b3a4554e72b25fc33626870a8781c07639f
Ng'ash_254 <samuelnganga.w@gmail.com>
2024-01-10 14:27:17 +0300

    Logout with Bloc

--- notes/lib/views/notes/notes_view.dart
+++ notes/lib/views/notes/notes_view.dart
@@ -1,7 +1,10 @@
 import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
 import "package:notes/constants/routes.dart";
 import "package:notes/enums/menu_action.dart";
 import "package:notes/services/auth/auth_service.dart";
+import "package:notes/services/auth/bloc/auth_bloc.dart";
+import "package:notes/services/auth/bloc/auth_event.dart";
 import "package:notes/services/cloud/cloud_note.dart";
 import "package:notes/services/cloud/firebase_cloud_storage.dart";
 import "package:notes/utilities/dialogs/logout_dialog.dart";
@@ -43,12 +46,8 @@ class _NotesViewState extends State<NotesView> {
                 case MenuAction.logout:
                   final shouldLogout = await showLogOutDialog(context);
                   if (shouldLogout) {
-                    await AuthService.firebase().logOut();
                     // ignore: use_build_context_synchronously
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      loginRoute,
-                      (route) => false,
-                    );
+                    context.read<AuthBloc>().add(const AuthEventLogout());
                   }
               }
             },
