500392da679d42554c3a230b036246cfd3f87350
Ng'ash_254 <samuelnganga.w@gmail.com>
2024-01-11 03:27:46 +0300

    Use bloc consumer

--- notes/lib/views/login_view.dart
+++ notes/lib/views/login_view.dart
@@ -5,7 +5,6 @@ import 'package:notes/services/auth/bloc/auth_bloc.dart';
 import 'package:notes/services/auth/bloc/auth_event.dart';
 import 'package:notes/services/auth/bloc/auth_state.dart';
 import 'package:notes/utilities/dialogs/error_dialog.dart';
-import 'package:notes/utilities/dialogs/loading_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
